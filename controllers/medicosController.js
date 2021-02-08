7bbb0f7c0348db903dc3677c19cc40363ae8cc43
deton-esc5 <dmeverton@gmail.com>
2021-02-07 23:39:17 -0300

    Correção da lógica de contagem de páginas na lsita de médicos cadastrados

--- controllers/medicosController.js
+++ controllers/medicosController.js
@@ -8,9 +8,9 @@ index: async (req, res) =>{
     let {page = 1} = req.query
     let {count:total, rows:medicos} = await Create_medicos.findAndCountAll({
         limit:7,
-        offset:(page - 1) * 7
+        offset:(page-1) * 7
     });
-    let totalPagina =Math.round(total/7)
+    let totalPagina =Math.round(total/7)+1
     return res.render('index', {medicos, totalPagina})
 },
    
